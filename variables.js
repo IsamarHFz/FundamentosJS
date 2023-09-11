1b4acabde58db46b959a468bc56cb4c65589711a
IsabelHernandez <marisahfz07@gmail.com>
2023-09-11 11:31:09 -0600

    03 Variables

---
+++ variables.js
@@ -0,0 +1,24 @@
+// Versiones anteriores a ES6 ers var 
+var miVariable;
+
+// de ES6 en adelante 
+// const y let
+
+// const declara constantes, mo permite reasignación 
+// Al crearla debe iniciaizarse 
+const PI = 3.14;
+// PI =3.28;
+
+// let declara variables 
+// permite reasignación 
+// se puede declarar y luego inicializar
+// JS detecta automaticamente el tipo de dato
+let firstname; 
+console.log(firstname);
+firstname = 'Peter Parker'
+console.log(firstname);
+console.log(typeof(firstname));
+
+firstname = 18; 
+console.log(firstname);
+console.log(typeof(firstname));
